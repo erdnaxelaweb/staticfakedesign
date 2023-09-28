bb368ac598d6eb6bed32e71bab069be00db9d34d
Florian ALEXANDRE <f.alexandre@novactive.com>
2023-09-28 23:38:06 +0200

    feat: coordinates to string method

--- lib/Value/Coordinates.php
+++ lib/Value/Coordinates.php
@@ -18,5 +18,8 @@ class Coordinates
     {
     }
 
-
+    public function __toString(): string
+    {
+        return sprintf('%s (%s / %s)', $this->address, $this->latitude, $this->longitude);
+    }
 }
