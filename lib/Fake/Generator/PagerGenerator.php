74ea2a0208fa5b41aafc50fd15276d60576b1b29
Florian ALEXANDRE <f.alexandre@novactive.com>
2024-06-14 14:09:58 +0200

    fix: pager not supporting page change

--- lib/Fake/Generator/PagerGenerator.php
+++ lib/Fake/Generator/PagerGenerator.php
@@ -18,11 +18,13 @@ use ErdnaxelaWeb\StaticFakeDesign\Fake\AbstractGenerator;
 use ErdnaxelaWeb\StaticFakeDesign\Fake\FakerGenerator;
 use ErdnaxelaWeb\StaticFakeDesign\Value\Pager;
 use ErdnaxelaWeb\StaticFakeDesign\Value\PagerAdapter;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PagerGenerator extends AbstractGenerator
 {
     public function __construct(
+        protected RequestStack $requestStack,
         protected ContentGenerator $contentGenerator,
         protected SearchFormGenerator $searchFormGenerator,
         protected LinkGenerator $linkGenerator,
@@ -47,13 +49,14 @@ class PagerGenerator extends AbstractGenerator
 
     public function __invoke(string $type, ?int $pagesCount = null): Pager
     {
+        $currentPage = (int) $this->requestStack->getCurrentRequest()->query->get('page', 1);
         $configuration = $this->pagerConfigurationManager->getConfiguration($type);
         $sorts = $configuration['sorts'];
         $filters = $configuration['filters'];
         $contentTypes = $configuration['contentTypes'];
         $maxPerPage = $configuration['maxPerPage'];
         $headlineCount = $configuration['headlineCount'];
-        $pagesCount = $pagesCount ?? rand(1, 10);
+        $pagesCount = $pagesCount ?? rand($currentPage, 10);
 
         $adapter = new PagerAdapter(
             static function () use ($maxPerPage, $pagesCount) {
@@ -79,6 +82,7 @@ class PagerGenerator extends AbstractGenerator
             }
         );
         $pager = new Pager($adapter);
+        $pager->setCurrentPage($currentPage);
         $pager->setMaxPerPage($maxPerPage);
         $pager->setHeadlineCount($headlineCount);
 
